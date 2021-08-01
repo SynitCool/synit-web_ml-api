389eaf85178871c19e6dc8c2f95dd5ac6c22dbb6
BASI <zolarpixel3@gmail.com>
2021-08-01 16:36:50 +0700

    edit app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import flask
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
 
+URL_WEB = 'https://synit-web.herokuapp.com/'
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return redirect(URL_WEB)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
