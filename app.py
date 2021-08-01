e2c4bd7afd6aeca54a3faa06d5d0d463fe44f620
BASI <zolarpixel3@gmail.com>
2021-08-01 16:11:17 +0700

    make ml API

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask, jsonify, request, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
