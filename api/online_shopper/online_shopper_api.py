88fa45a60efb465dc3134fb1c094928be144c553
BASI <zolarpixel3@gmail.com>
2021-10-26 19:48:15 +0700

    edit api

--- api/online_shopper/online_shopper_api.py
+++ api/online_shopper/online_shopper_api.py
@@ -4,27 +4,33 @@ from flask import Blueprint
 from flask import jsonify
 from flask import request
 
-from .online_shopper.preprocessing import load_object
+from .preprocessing import load_object
 
-from .online_shopper.config import X_OBJECT_COLUMNS
-from .online_shopper.config import X_OBJECT_INDEX
+from .config import X_OBJECT_COLUMNS
+from .config import X_OBJECT_INDEX
+
+import warnings
 
 online_shopper_api = Blueprint("online_shopper_api", __name__)
 
+
 @online_shopper_api.route("/online_shopper_api", methods=["POST"])
 def online_shopper_model():
+    # Handle warnings
+    warnings.filterwarnings("ignore", category=UserWarning)
+
     # loading json
     data = request.get_json(force=True)
 
     X_data = data["input"]
 
     # declare model
-    model_path = "api/object_save/model_object_rfc"
+    model_path = "api/online_shopper/object_save/model_object_rfc"
     model = load_object(model_path)
 
     # cleaning data
     for index, column in zip(X_OBJECT_INDEX, X_OBJECT_COLUMNS):
-        file_path = f"api/object_save/encoder_columns_{column}"
+        file_path = f"api/online_shopper/object_save/encoder_columns_{column}"
         le = load_object(file_path)
 
         X_data[index] = le.transform([X_data[index]])
@@ -35,9 +41,8 @@ def online_shopper_model():
     y_pred = model.predict(X_data)
 
     # encode label
-    file_path = "api/object_save/encoder_columns_label"
+    file_path = "api/online_shopper/object_save/encoder_columns_label"
     le = load_object(file_path)
     y_pred = le.transform([y_pred[0]])
 
     return jsonify(results=str(y_pred[0]))
-    return "<h1>Online Shopper API</h1>"
